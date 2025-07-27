fb76efdb84134d4669c21065763ae101a7e599b7
vibhu gupta <vibhugupta022@gmail.com>
2025-07-27 16:44:48 +0530

    Update Home.jsx

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-import front from "../assets/front.jpg";
+import front from "../assets/Front.jpg";
 import TiltedCard from "./TiltedCard.jsx";
 
 function Home() {
