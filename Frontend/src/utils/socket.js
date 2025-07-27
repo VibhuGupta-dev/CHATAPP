153ac080150ea3db56c56323ed85b1708c6c61ef
vibhu gupta <vibhugupta022@gmail.com>
2025-07-27 18:40:04 +0530

    Update socket.js

--- Frontend/src/utils/socket.js
+++ Frontend/src/utils/socket.js
@@ -4,7 +4,7 @@ let socket;
 
 export const connectSocket = () => {
     if (!socket) {
-        socket = io("http://localhost:5000", {
+        socket = io("https://chatapp-backend-1-sgnx.onrender.com", {
             transports: ["websocket"],
         });
 
